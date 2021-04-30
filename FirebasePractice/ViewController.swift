a245ff3e9474358a5f852fa1150247b0ad3dcbb6
David Sadler <davidwallacesadler@gmail.com>
2021-04-29 21:12:45 -0600

    Inital Commit

---
+++ FirebasePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebasePractice
+//
+//  Created by David Sadler on 4/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
