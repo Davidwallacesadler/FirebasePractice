1b7cc7700fd09df209ae356474bca50482ff15a3
David Sadler <davidwallacesadler@gmail.com>
2021-05-04 00:21:11 -0600

    Init firebase instance and pods

--- FirebasePractice/ViewController.swift
+++ FirebasePractice/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
+class ViewController: UIViewController {
+    
+    // MARK: - Local Properties:
+    // Local firestore db instance:
+    var db: Firestore!
+
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Firebase setup:
+        let settings = FirestoreSettings()
+        Firestore.firestore().settings = settings
+        db = Firestore.firestore()
+        
+        // Firebase Crud practice:
+//        createMocWorkout()
     }
 
+    // MARK: - CRUD Practice:
+    private func createMocWorkout() {
+        // Define the shape of the data:
+        let workoutData : [String: Any] = [
+            "workouts":
+                [
+                    [
+                        "week_day": "Monday",
+                        
+                    ],
+                
+                ],
+            "start_date": Timestamp(date: Date()),
+            "end_date": Timestamp(date: Date()),
+        ]
+        
+        // Grab the document reference:
+        var reference: DocumentReference? = nil
+        
+        // Use the reference to add a collection / document
+        reference = db.collection("workouts").addDocument(data: workoutData) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(reference!.documentID)")
+            }
+        }
+    }
 
 }
 
